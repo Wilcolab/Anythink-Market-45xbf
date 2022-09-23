618ec3cc3a4966874a1f52e98e6bc8a116d35f65
Matthew Festa <festa.matthew@me.com>
2022-09-23 10:14:47 -0400

    reverted to original

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -36,7 +36,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={frontend.images.placeholder.png}
+        src={item.image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
